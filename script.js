c5e47901a9c827d1447e2a6fc5f522819dfc6048
amitgiri257 <127419759+amitgiri257@users.noreply.github.com>
2023-08-24 20:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const lookup = {
 		else decodedArr.push(lookup[str[i]])
 	}
  
-  return; //return decodedArr
+  return decodedArr;
 }
   // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
   
